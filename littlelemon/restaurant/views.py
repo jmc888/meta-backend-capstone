537ab69a4ffa086b54e2b8861041dba595c7bfc7
Jiamin <jmchen@utexas.edu>
2024-06-20 20:52:54 -0700

    add booking api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .serializers import BookingSerializer
+from .models import Booking
+from rest_framework import viewsets, permissions
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
