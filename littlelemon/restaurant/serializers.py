5cc5b23e6bc3bc5094d222e95f121b6be86e8dd2
Jiamin <jmchen@utexas.edu>
2024-06-22 03:03:00 -0700

    simplify user registration

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -17,19 +17,11 @@ class MenuItemSerializer(serializers.ModelSerializer):
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(style={"input_type": "password"}, write_only=True)
-    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2"]
+        fields = ["username", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
-
-    def validate(self, data):
-        if data["password"] != data["password2"]:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."}
-            )
-        return data
 
     def create(self, validated_data):
         user = User.objects.create(
